10a8e87ff5db6b6337264296c7fc4d6e9ce33a7a
Isaac Ayomide <okunlolaa3@outlook.com>
2024-06-27 22:40:23 +0100

    fix: empty input fields solved

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const cardNoDisplay = document.querySelector(".card__no");
 const cardNameDisplay = document.querySelector(".card__name");
 const expMonthDisplay = document.querySelector(".exp__month");
 const expYearDisplay = document.querySelector(".exp__year");
-const errorMsg = document.querySelector(".error__msg");
+const errorMsg = document.querySelectorAll(".error__msg");
 const cardHolderNameErr = document.querySelector(".cardholdername__err");
 const cardHolderNumberErr = document.querySelector(".cardholdernumber__err");
 const cardMonthErr = document.querySelector(".cardmonth__err");
@@ -22,3 +22,31 @@ const cvcErr = document.querySelector(".cvc__err");
 // Buttons
 const submitBtn = document.querySelector(".submit__btn");
 const proceedBtn = document.querySelector(".proceed__btn");
+
+const emptyFields = function (inputEl, errorEl, errorMsg = "") {
+  if (inputEl.value.trim().length === 0) {
+    errorEl.style.display = "block";
+    errorEl.style.color = "red";
+    inputEl.style.border = "1px solid red";
+  } else {
+    errorEl.style.display = "none";
+    inputEl.style.border = "1px solid #dfdee0";
+  }
+
+  if (errorMsg) {
+    errorEl.textContent = errorMsg;
+  }
+};
+
+const validateFields = function () {
+  emptyFields(cardNameInput, cardHolderNameErr, "Cannot be blank");
+  emptyFields(cardNumberInput, cardHolderNumberErr, "Cannot be blank");
+  emptyFields(cardMonthInput, cardMonthErr, "Cannot be blank");
+  emptyFields(cardYearInput, cardYearErr, "Cannot be blank");
+  emptyFields(cvcInput, cvcErr, "Cannot be blank");
+};
+
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  validateFields();
+});
